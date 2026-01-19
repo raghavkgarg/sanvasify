2860933b6f549ea82f84dddcf30790a954b33581
Gaurav Garg <gkgarg24@yahoo.com>
2026-01-18 20:54:35 -0500

    1) Create new package pkg/api for webserver code 2) Refactor out handlers, routes, server and store in to their own files 3) Clean up main 4) Fix spell mistake in index.html 5) Add a TODO and design file in etc/

---
+++ pkg/api/handlers.go
@@ -0,0 +1,65 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (s *Server) handleSchemes() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		schemes := s.store.GetAllSchemes()
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		json.NewEncoder(w).Encode(schemes)
+	}
+}
+
+func (s *Server) handleNAV() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			http.Error(w, "Missing scheme code", http.StatusBadRequest)
+			return
+		}
+
+		found := s.store.GetSchemeByCode(code)
+		if found == nil {
+			http.Error(w, "Scheme not found", http.StatusNotFound)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		json.NewEncoder(w).Encode(found)
+	}
+}
+
+func (s *Server) handleFilters() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		options := s.store.GetFilterOptions()
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(options)
+	}
+}
+
+func (s *Server) handleSearch() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fType := r.URL.Query().Get("fund_type")
+		fStrategy := r.URL.Query().Get("fund_strategy")
+		fCompany := r.URL.Query().Get("fund_company")
+		dist := r.URL.Query().Get("distribution_option")
+		mode := r.URL.Query().Get("purchase_mode")
+
+		schemes := s.store.SearchSchemes(fType, fStrategy, fCompany, dist, mode)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		json.NewEncoder(w).Encode(schemes)
+	}
+}
